8f843bded55cc7ceae42397575e18581b0807b2d
Anubis-cmyk <it19099132@my.sliit.lk>
2021-11-26 21:04:31 +0530

    inisial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React,{FC} from 'react'
+import logo from './logo.svg';
+import './App.css';
+
+const App:FC =()=> {
+  return (
+    <div className="App">
+      
+      
+    </div>
+  );
+}
+
+export default App;
